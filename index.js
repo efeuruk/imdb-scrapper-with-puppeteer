b3d6ca89738703019accc6ad642b81e0ba52279a
Ersoy Efe Uruk <efeuruk1@gmail.com>
2024-02-04 00:34:06 +0300

    Updated selectors with new ones and added year parameter

--- index.js
+++ index.js
@@ -5,61 +5,77 @@
 	Usage: you can use: yarn scrap:imdb <movie-name> or npm run scrap:imdb <movie-name>
 	ex: yarn scrap godfather
 */
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-	const imdb = 'https://www.imdb.com';
-	const args = process.argv.slice(2);
-	if (args.length < 1) {
-		throw new Error('You should enter a movie name');
-	}
-	const movie = args.join(' ');
+  const imdb = "https://www.imdb.com";
+  const args = process.argv.slice(2);
+  if (args.length < 1) {
+    throw new Error("You should enter a movie name");
+  }
+  const movie = args.join(" ");
 
-	// Open up the browser, { headless: false }
-	const browser = await puppeteer.launch({ headless: false });
+  // Open up the browser, { headless: false }
+  const browser = await puppeteer.launch({ headless: false });
 
-	// Create a new tab
-	const page = await browser.newPage();
+  // Create a new tab
+  const page = await browser.newPage();
 
-	// Tell browser the navigation is finished
-	// when there are more than 2 network connections for at least half of a second
-	await page.goto(imdb, { waitUntil: 'networkidle2' });
+  // Tell browser the navigation is finished
+  // when there are more than 2 network connections for at least half of a second
+  await page.goto(imdb, { waitUntil: "networkidle2" });
 
-	// Type movie into search bar
-	await page.type('.imdb-header-search__input', movie);
+  // Selectors
+  const SEARCH_INPUT_CLASS = ".imdb-header-search__input";
 
-	// Wait for the selector to appear in page
-	await page.waitForSelector('#react-autowhatever-1--item-0 a');
+  // Type movie into search bar
+  await page.type(SEARCH_INPUT_CLASS, movie);
 
-	// Take the first result's href
-	const href = await page.evaluate(
-		() => document.querySelector('#react-autowhatever-1--item-0 a').href
-	);
+  // Wait for the selector to appear in page
+  await page.waitForSelector(
+    "#react-autowhatever-navSuggestionSearch--item-0 a"
+  );
 
-	// Go to that url
-	await page.goto(href, { waitUntil: 'networkidle2' });
+  // Take the first result's href
+  const href = await page.evaluate(
+    () =>
+      document.querySelector(
+        "#react-autowhatever-navSuggestionSearch--item-0 a"
+      ).href
+  );
 
-	// Run console code inside here, dom functions etc.
-	const data = await page.evaluate(() => {
-		const getText = (selector) => {
-			if (document.querySelector(selector)) {
-				return document.querySelector(selector).innerText;
-			}
-			return "There is no value for this prop";
-		}
+  // Go to that url
+  await page.goto(href, { waitUntil: "networkidle2" });
 
-		const title = getText('div[class="title_wrapper"] > h1');
-		const rating = getText('span[itemprop="ratingValue"]');
-		const duration = getText('.titleBar time');
+  // Run console code inside here, dom functions etc.
+  const data = await page.evaluate(() => {
+    const getText = (selector, all = false) => {
+      if (all && document.querySelectorAll(selector)) {
+        return Array.from(document.querySelectorAll(selector)).map(
+          el => el.innerText
+        );
+      } else if (document.querySelector(selector)) {
+        return document.querySelector(selector).innerText;
+      }
+      return "There is no value for this prop";
+    };
 
-		return {
-			title,
-			rating,
-			duration,
-		};
-	});
+    const title = getText("h1");
+    const rating = getText(
+      'div[data-testid="hero-rating-bar__aggregate-rating__score"]'
+    );
+    const duration = getText("li.ipc-inline-list__item", true)[6];
+    const year = getText("li.ipc-inline-list__item", true)[4];
 
-	console.log(data);
+    return {
+      title,
+      rating,
+      duration,
+      year,
+    };
+  });
 
-	await browser.close();
+  console.log(data);
+
+  await browser.close();
 })();
